4c0b8b7fb11a352b55c9997443f2db4cc9784473
Kleider Gonzalez <kgonzalez@wocex.io>
2022-03-02 12:34:23 -0600

    [ADD] Carpeta de enums -> estatus de publicaciones

---
+++ src/enums/statusEnum.ts
@@ -0,0 +1,10 @@
+/**
+ * Enumerador para estatus de los posts
+ * @enum statusEnum
+ */
+ export enum statusEnum {
+    drafted = "drafted",
+    deleted = "deleted",
+    published = "published",
+  }
+  
